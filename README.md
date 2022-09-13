aea6cc3a3a8d4039a4fe8a882c509da435316f68
Maze Token <68662262+mazetoken@users.noreply.github.com>
2022-09-13 21:35:32 +0200

    add stake contract

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # The Book of Dog - blockchain guestbook on Dogechain
 
 The Book of Dog is [here](https://bookofdog.doge.wf)
+
+Send a blockchain message and mint BODO tokens
+
+Stake BODO and receive BLOCKCHAIN INVADERS tokens
--- functions.js
+++ functions.js
@@ -10,7 +10,7 @@ function sendMessage() {
     var content = "Sending transaction from: ";
     content += dogMaster;
     $("#lang").html(content);
-    var event = contractTheBookOfDog.methods.sendMessage(sender, message).send({ from: dogMaster, value: 50000000000000000 })
+    var event = contractTheBookOfDog.methods.sendMessage(sender, message).send({ from: dogMaster, value: 25000000000000000 })
         .then(function (receipt) {
             console.log(receipt);
     var content = "Transaction sent! You got 550 BODO: <br>";
@@ -87,4 +87,73 @@ function allMessages2() {
     $("#lang6").html(content);
        });;
     }
+};
+
+// Stake
+function approve() {
+    var amount1 = $("#amount1").val();
+    var amount2 = amount1 *100000000;
+    var amount3 = amount2.toString();
+    var content = "Approving transaction from: ";
+    content += dogMaster;
+    $("#lang7").html(content);
+    var event = contractTheBookOfDog.methods.approve("0x2efDE281498838C5405b4284d87aC319117e9B3C", amount3).send({ from: dogMaster })
+        .then(function (receipt) {
+            console.log(receipt);
+    var content = "Approved!: ";
+            //alert("Done. BODO approved!")
+    content += JSON.stringify(receipt.transactionHash);
+    $("#lang7").html(content);
+        });;
+};
+
+function stakeTokens() {
+    var amount1 = $("#amount1").val();
+    var amount2 = amount1 *100000000;
+    var amount3 = amount2.toString();
+    var content = "Sending transaction from: ";
+    content += dogMaster;
+    $("#lang7").html(content);
+    var event = contractTheBookOfDogStake.methods.stakeTokens(amount3).send({ from: dogMaster })
+        .then(function (receipt) {
+            console.log(receipt);
+    var content = "Transaction sent! Staked!: ";
+            //alert("Done. BODO staked!");
+    content += JSON.stringify(receipt.transactionHash);
+    $("#lang7").html(content);
+        });;
+};
+    
+function stakingBalance() {
+    var event = contractTheBookOfDogStake.methods.stakingBalance(dogMaster).call()
+        .then(function (result) {
+    var content = "Your BODO balance staked is: ";
+            //alert(result / 100000000);
+    content += JSON.stringify(result.toString() / 100000000);
+    $("#lang8").html(content);
+        });;
+};
+
+function calculateReward() {
+    var event = contractTheBookOfDogStake.methods.calculateReward().call({ from: dogMaster })
+        .then(function (result) {
+    var content = "Your current INVADERS reward is: ";
+            //alert(result / 100000000);
+    content += JSON.stringify(result.toString() / 1000000000000000000);
+    $("#lang9").html(content);
+        });;
+};
+    
+function unstakeTokens() {
+    var content = "Sending transaction from: ";
+    content += dogMaster;
+    $("#lang10").html(content);
+    var event = contractTheBookOfDogStake.methods.unstakeTokens().send({ from: dogMaster })
+        .then(function (receipt) {
+            console.log(receipt);
+    var content = "Transaction sent! ";
+            //alert("Done. BODO and INVADERS sent!");
+    content += JSON.stringify(receipt.transactionHash);
+    $("#lang10").html(content);
+        });;
 };
